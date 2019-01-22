cafd8a6881ef077ecfba2a1e429797a92ba28766
Fanny Huang <>
2019-01-22 19:18:46 +0100

    Put all logistic regression related functions into a class

---
+++ LogisticRegressionOVA.py
@@ -0,0 +1,125 @@
+# coding: utf-8
+
+import math
+import numpy as np
+import pandas as pd
+
+class LogisticRegressionOVA():
+
+	def __init__(self, n_iter = 1000, learning_rate = 12):
+		self.n_iter = n_iter
+		self.learning_rate = learning_rate
+		self.cost_history = []
+		self.weights = []
+		self.classifier = []
+
+
+	def __sigmoid(self, z):
+		'''
+		z: (m, 1)
+
+		Sigmoid = 1 / 1e(-z)
+		'''
+		return np.array([1 / (1 + math.exp(-x)) for x in z])
+
+
+	def __predict(self, X, weights):
+		return self.__sigmoid(np.dot(X, weights))
+
+
+	def __accuracy(self, my_y, his_y):
+		diff = his_y - my_y
+		return 1 - (np.count_nonzero(diff)) / len(diff)
+
+
+	def __decision_boundary(self, probability):
+		return 1 if probability > 0.5 else 0
+
+	def __cost_function(self, X, y):
+		'''
+		Using Cross-Entropy / Log Cross
+
+		X: (m, 1)
+		y: (m, 1)
+		weights: (n, 1)
+		Returns 1D matrix of predictions
+
+		Cost = (y * log(predictions) + (1-y)*log(1-predictions)) / len(X)
+		'''
+
+		predictions = self.__predict(X, self.weights)
+		cost_1 = -y * np.log(predictions)
+		cost_2 = (1 - y) * np.log(1 - predictions)
+		return (cost_1 - cost_2).sum() / len(X)
+
+
+	def __update_weights(self, X, y):
+		'''
+		X: (m, 1)
+		y: (m, 1)
+		weights: (n, 1)
+		'''
+
+		predictions = self.__predict(X, self.weights)
+		gradient = np.dot(X.T, predictions - y)
+		self.weights = self.weights - gradient / len(X) * self.learning_rate
+
+
+	def __fit(self, X, y):
+		for _ in range(self.n_iter):
+			self.__update_weights(X, y)
+			cost = self.__cost_function(X, y)
+			self.cost_history.append(cost)
+		return self.weights
+
+
+	def save_weights_into(self, path):
+		np.save(path, self.weights)
+		return self
+
+	def save_classifier_into(self, path):
+		np.save(path, self.classifier)
+		return self
+
+
+	def set_weights(self, weights):
+		self.weights = weights
+		return self
+
+
+	def set_classifier(self, classifier):
+		self.classifier = classifier
+		return self
+
+
+	def fit(self, X, y, one_vs_all=None):
+		self.weights = np.array([0.0] * len(X.columns))
+		if one_vs_all:
+			self.classifier = [self.__fit(X, np.where(y == one, 1, 0)) for one in one_vs_all]
+		else:
+			self.classifier = self.__fit(X, y)
+		return self
+
+
+	def predict(self, X, one_vs_all=None):
+		if (one_vs_all == None and len(self.weights) == 0) or (one_vs_all and len(self.classifier) == 0):
+			raise Error("Fit first then predict.")
+		if one_vs_all:
+			return [
+				[self.__decision_boundary(prob) for prob in self.__predict(X, self.classifier[i])]
+				for i, one in enumerate(one_vs_all)
+			]
+		else:
+			return [self.__decision_boundary(prob) for prob in self.__predict(X, self.weights)]
+
+
+	def score(self, X, y, one_vs_all=None):
+		if one_vs_all:
+			predictions = self.predict(X, one_vs_all)
+			return [
+				self.__accuracy(predictions[i], np.where(y == one, 1, 0))
+				for i, one in enumerate(one_vs_all)
+			]
+		else:
+			predictions = self.predict(X)
+			return self.__accuracy(predictions, y)
--- logreg_train.py
+++ logreg_train.py
@@ -1,114 +1,22 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
-import sys
-import math
 import numpy as np
 import pandas as pd
-from hp_tools import get_houses, get_disciplines, get_features
+from hp_tools import get_houses, get_features
 from tools import read_file, normalize_df
-
-## J(θ) = − (1/m) sum( yi log(hθ(xi)) + (1 − yi) log(1 − hθ(xi)))
-## hθ(x) = g(θT x)
-## g(z) = 1 / 1+e−z
-## ∂ / ∂θj J(θ) =  1/m sum(hθ(xi) − yi)xij m i=1
-
-def save_weights_into(path, weights):
-	np.save(path, weights)
-
-
-def accuracy(my_y, his_y):
-	diff = his_y - my_y
-	return 1 - (np.count_nonzero(diff)) / len(diff)
-
-
-def decision_boundary(probability):
-	return 1 if probability > 0.5 else 0
-
-
-def sigmoid(z):
-	'''
-	z: (m, 1)
-
-	Sigmoid = 1 / 1e(-z)
-	'''
-	return np.array([1 / (1 + math.exp(-x)) for x in z])
-
-
-def predict(X, weights):
-	return sigmoid(np.dot(X, weights))
-
-
-def cost_function(X, y, weights):
-	'''
-	Using Cross-Entropy / Log Cross
-
-	X: (m, 1)
-	y: (m, 1)
-	weights: (n, 1)
-	Returns 1D matrix of predictions
-
-	Cost = (y * log(predictions) + (1-y)*log(1-predictions)) / len(X)
-	'''
-
-	predictions = predict(X, weights)
-	cost_1 = -y * np.log(predictions)
-	cost_2 = (1 - y) * np.log(1 - predictions)
-	return (cost_1 - cost_2).sum() / len(X)
-
-
-def update_weights(X, y, weights):
-	'''
-	X: (m, 1)
-	y: (m, 1)
-	weights: (n, 1)
-	'''
-	learning_rate = 12
-
-	predictions = predict(X, weights)
-	gradient = np.dot(X.T, predictions - y)
-	return weights - gradient / len(X) * learning_rate
-
-
-def train(X, y, current_train):
-	cost_history = []
-	weights = np.array([0.0] * len(X.columns))
-	yh = np.where(y == current_train, 1.0, 0.0)
-	for _ in range(1000):
-		weights = update_weights(X, yh, weights)
-		cost = cost_function(X, yh, weights)
-		cost_history.append(cost)
-	return {'weights': weights, 'cost_history': cost_history}
-
-
-def test(X_test, y_test, weights):
-	predictions = [decision_boundary(prob) for prob in predict(X_test, weights)]
-	return accuracy(predictions, y_test)
-
-
-def one_vs_all(df, y_label, features, all):
-	X = df.loc[:, features]
-	X_test = X.sample(frac=0.3)
-	X_train = X.loc[X.index.difference(X_test.index.values)]
-	y_test, y_train = df.loc[X_test.index.values][y_label], df.loc[X_train.index.values][y_label]
-
-	ret = {key: None for key in all}
-	for one in all:
-		ret[one] = train(X_train, y_train, one)
-
-	for one in all:
-		yh_test = np.where(y_test == one, 1, 0)
-		accuracy = test(X_test, yh_test, np.array(ret[one]['weights']))
-		print("for {}, accuracy of {}".format(one, accuracy))
-	return [ret[one]['weights'] for one in all]
+from LogisticRegressionOVA import LogisticRegressionOVA
 
 
 if __name__ == "__main__":
-
 	features = get_features()
-
 	file = "res/dataset_train.csv"
 	dataset = read_file(file, ignore=True)
 	df = normalize_df(pd.DataFrame(dataset[1:], columns=dataset[0]))
-	weights = one_vs_all(df, 'Hogwarts House', features, get_houses())
-	save_weights_into("weights", weights)
+
+	X = df.loc[:, features]
+	y = df["Hogwarts House"]
+
+	all = get_houses()
+	log_reg = LogisticRegressionOVA().fit(X, y, one_vs_all=all)
+	log_reg.save_classifier_into("classifier.npy")
