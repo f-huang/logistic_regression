149f33e567e5dc76efa45a49f0cd5c832d11a3de
Fanny HUANG <fhuang@e3r3p18.42.fr>
2019-01-23 11:55:49 +0100

    Clean a bit

--- tools.py
+++ tools.py
@@ -14,36 +14,12 @@ def is_number(string):
 		return False
 
 
-def list_to_dict(list, key_index = 0):
-	return {
-		row[key_index]: row[1:]
-		for row in list
-	}
-
 def is_float(value):
 	try:
 		float(value)
 		return True
 	except ValueError:
 		return False
-
-
-def read_file(filename, ignore=False):
-	try:
-		with open(filename, 'r') as f:
-			reader = csv.reader(f, delimiter=',')
-			# next(reader)
-			return (list(filter(lambda row: None not in row,
-				[[float(item) if is_float(item) else (item if item else None)
-					for item in row
-				] for row in reader]))) if ignore\
-				else [[float(item) if item and is_float(item)\
-					else (item if item else 0.0)
-					for item in row]
-				for row in reader]
-	except IOError:
-		print("Cannot read this file: {}".format(filename))
-		sys.exit(-1)
 
 
 def normalize_list(list):
